293420b1abf3f20dba34b84c85a2fb5ada1bfd9c
Th0rn-dev <solnek33@gmail.com>
2021-03-25 12:28:52 +0300

    add checkeditor for field 'Content' in admin flatpages

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'ckeditor',
     'main'
 ]
 
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
