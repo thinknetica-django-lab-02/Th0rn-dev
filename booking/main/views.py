42fd322ddbc875dfd54143ee7f0c5d3480173590
Th0rn-dev <solnek33@gmail.com>
2021-03-31 09:56:21 +0300

    split the template into fragments

--- booking/main/views.py
+++ booking/main/views.py
@@ -5,9 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     turn_on_block = True
-    user = request.user
     context = {
         "turn_on_block": turn_on_block,
-        "user": user
     }
     return render(request, "main/index.html", context=context)
