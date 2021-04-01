4d6ee50297a85b9f5a798e36bf8ee1a0db012191
Th0rn-dev <solnek33@gmail.com>
2021-04-01 15:01:46 +0300

    add rooms list view and room detail view

--- booking/main/views.py
+++ booking/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Room
 
 
 def index(request):
@@ -9,3 +10,17 @@ def index(request):
         "turn_on_block": turn_on_block,
     }
     return render(request, "main/index.html", context=context)
+
+
+class RoomsList(ListView):
+    """Rooms views from generics"""
+    model = Room
+    template_name = "main/rooms_list.html"
+    context_object_name = "rooms"
+
+
+class RoomDetail(DetailView):
+    """Room detail"""
+    model = Room
+    context_object_name = "room"
+    queryset = Room.objects.all()
