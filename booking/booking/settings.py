bec55792096f3d160e6f07f6868b546c6ecc9615
Th0rn-dev <solnek33@gmail.com>
2021-04-26 22:03:58 +0300

    tune-up collectstatic

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static/'))
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
