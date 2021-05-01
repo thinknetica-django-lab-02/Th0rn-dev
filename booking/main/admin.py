f0de38f18ea7d192897f008aacf44a2cf01def4f
Th0rn-dev <solnek33@gmail.com>
2021-05-01 19:17:03 +0300

    groups tags and date created, action change status publishing and archive

--- booking/main/admin.py
+++ booking/main/admin.py
@@ -2,11 +2,24 @@ from django.contrib import admin
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from django.db import models
+from django.utils.html import format_html
 
 from ckeditor.widgets import CKEditorWidget
 
 from .models import (Room, AccommodationFacility, AccommodationManager,
                      Tag, Profile, Subscriber, SMSLog)
+
+
+def make_published(self, request, queryset):
+    queryset.update(status='p')
+
+
+def make_archived(self, request, queryset):
+    queryset.update(status='a')
+
+
+make_published.short_description = "Mark selected rooms as published"
+make_archived.short_description = "Mark selected rooms as archived"
 
 
 class CustomFlatPageAdmin(FlatPageAdmin):
@@ -19,9 +32,23 @@ admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, CustomFlatPageAdmin)
 
 
+class AccommodationFacilityInline(admin.TabularInline):
+    model = AccommodationFacility
+    extra = 1
+
+
 @admin.register(Room)
 class AdminRoom(admin.ModelAdmin):
-    pass
+    list_display = ("number", "hotel", "created", "status", "tags_list")
+    actions = [make_published, make_archived]
+    ordering = ['hotel']
+    list_filter = ('tags', 'created')
+
+    def tags_list(self, obj):
+        return format_html("<br />".join([tag.tag_name for tag in obj.tags.all()]))
+
+    tags_list.empty_value_display = "-"
+    tags_list.short_description = "Теги"
 
 
 @admin.register(AccommodationFacility)
@@ -31,7 +58,9 @@ class AdminAccommodationFacility(admin.ModelAdmin):
 
 @admin.register(AccommodationManager)
 class AdminAccommodationManager(admin.ModelAdmin):
-    pass
+    fields = (("last_name", "first_name", "middle_name"), "profile")
+    list_display = ("__str__", "profile")
+    inlines = [AccommodationFacilityInline]
 
 
 @admin.register(Tag)
@@ -52,3 +81,4 @@ class AdminSubscriber(admin.ModelAdmin):
 @admin.register(SMSLog)
 class AdminSMSLog(admin.ModelAdmin):
     pass
+
